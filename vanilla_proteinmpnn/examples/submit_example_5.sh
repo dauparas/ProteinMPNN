a989a67d1376c6fc825de38392424b0edcc3e32f
Justas Dauparas <justas.dauparas@gmail.com>
2022-06-05 13:36:22 -0700

    adding more examples

--- vanilla_proteinmpnn/examples/submit_example_5.sh
+++ vanilla_proteinmpnn/examples/submit_example_5.sh
@@ -3,13 +3,13 @@
 #SBATCH --mem=32g
 #SBATCH --gres=gpu:rtx2080:1
 #SBATCH -c 3
-#SBATCH --output=example_4.out
+#SBATCH --output=example_5.out
 
 source activate mlfold
 
 folder_with_pdbs="../PDB_complexes/pdbs/"
 
-output_dir="../PDB_complexes/example_4_outputs"
+output_dir="../PDB_complexes/example_5_outputs"
 if [ ! -d $output_dir ]
 then
     mkdir -p $output_dir
@@ -19,9 +19,10 @@ fi
 path_for_parsed_chains=$output_dir"/parsed_pdbs.jsonl"
 path_for_assigned_chains=$output_dir"/assigned_pdbs.jsonl"
 path_for_fixed_positions=$output_dir"/fixed_pdbs.jsonl"
+path_for_tied_positions=$output_dir"/tied_pdbs.jsonl"
 chains_to_design="A C"
-#The first amino acid in the chain corresponds to 1 and not PDB residues index for now.
-fixed_positions="1 2 3 4 5 6 7 8 23 25, 10 11 12 13 14 15 16 17 18 19 20 40" #fixing/not designing residues 1 2 3...25 in chain A and residues 10 11 12...40 in chain C
+fixed_positions="9 10 11 12 13 14 15 16 17 18 19 20 21 22 23, 10 11 18 19 20 22"
+tied_positions="1 2 3 4 5 6 7 8, 1 2 3 4 5 6 7 8" #two list must match in length; residue 1 in chain A and C will be sampled togther;
 
 python ../helper_scripts/parse_multiple_chains.py --input_path=$folder_with_pdbs --output_path=$path_for_parsed_chains
 
@@ -29,10 +30,13 @@ python ../helper_scripts/assign_fixed_chains.py --input_path=$path_for_parsed_ch
 
 python ../helper_scripts/make_fixed_positions_dict.py --input_path=$path_for_parsed_chains --output_path=$path_for_fixed_positions --chain_list "$chains_to_design" --position_list "$fixed_positions"
 
+python ../helper_scripts/make_tied_positions_dict.py --input_path=$path_for_parsed_chains --output_path=$path_for_tied_positions --chain_list "$chains_to_design" --position_list "$tied_positions"
+
 python ../protein_mpnn_run.py \
         --jsonl_path $path_for_parsed_chains \
         --chain_id_jsonl $path_for_assigned_chains \
         --fixed_positions_jsonl $path_for_fixed_positions \
+        --tied_positions_jsonl $path_for_tied_positions \
         --out_folder $output_dir \
         --num_seq_per_target 2 \
         --sampling_temp "0.1" \
--- vanilla_proteinmpnn/examples/submit_example_7.sh
+++ vanilla_proteinmpnn/examples/submit_example_7.sh
@@ -3,13 +3,13 @@
 #SBATCH --mem=32g
 #SBATCH --gres=gpu:rtx2080:1
 #SBATCH -c 3
-#SBATCH --output=example_4.out
+#SBATCH --output=example_7.out
 
 source activate mlfold
 
 folder_with_pdbs="../PDB_complexes/pdbs/"
 
-output_dir="../PDB_complexes/example_4_outputs"
+output_dir="../PDB_complexes/example_7_outputs"
 if [ ! -d $output_dir ]
 then
     mkdir -p $output_dir
@@ -17,23 +17,24 @@ fi
 
 
 path_for_parsed_chains=$output_dir"/parsed_pdbs.jsonl"
-path_for_assigned_chains=$output_dir"/assigned_pdbs.jsonl"
-path_for_fixed_positions=$output_dir"/fixed_pdbs.jsonl"
-chains_to_design="A C"
-#The first amino acid in the chain corresponds to 1 and not PDB residues index for now.
-fixed_positions="1 2 3 4 5 6 7 8 23 25, 10 11 12 13 14 15 16 17 18 19 20 40" #fixing/not designing residues 1 2 3...25 in chain A and residues 10 11 12...40 in chain C
+path_for_assigned_chains=$output_dir"/PDB_complexes/assigned_pdbs.jsonl"
+path_for_bias=$output_dir"/bias_pdbs.jsonl"
+AA_list="G P A"
+bias_list="40.1 0.3 -0.05" #for G P A respectively; global AA bias in the logit space
+chains_to_design="A B"
+
 
 python ../helper_scripts/parse_multiple_chains.py --input_path=$folder_with_pdbs --output_path=$path_for_parsed_chains
 
 python ../helper_scripts/assign_fixed_chains.py --input_path=$path_for_parsed_chains --output_path=$path_for_assigned_chains --chain_list "$chains_to_design"
 
-python ../helper_scripts/make_fixed_positions_dict.py --input_path=$path_for_parsed_chains --output_path=$path_for_fixed_positions --chain_list "$chains_to_design" --position_list "$fixed_positions"
+python ../helper_scripts/make_bias_AA.py --output_path=$path_for_bias --AA_list="$AA_list" --bias_list="$bias_list"
 
 python ../protein_mpnn_run.py \
         --jsonl_path $path_for_parsed_chains \
         --chain_id_jsonl $path_for_assigned_chains \
-        --fixed_positions_jsonl $path_for_fixed_positions \
         --out_folder $output_dir \
+        --bias_AA_jsonl $path_for_bias \
         --num_seq_per_target 2 \
         --sampling_temp "0.1" \
         --batch_size 1
